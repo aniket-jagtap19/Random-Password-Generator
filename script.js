40d9c250804bb4660637018f1729ef1fe63ee1b9
Aniket Jagtap <jagtapaniket731712@gmail.com>
2025-07-12 23:17:24 +0530

    Initial

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const charss = "0123456789abcdef" ;
-        function clickme(){
-           
-            let txt = [] ;
-            for(let i = 0 ; i < 6 ; i++){
-                const rand2 = Math.floor(Math.random()*16);
-                txt.push(`${charss[rand2]}`);
-            }
-            txt = txt.join("") ;
-          //  console.log(txt);
-            document.getElementById("h4").textContent=`#` + `${txt}`;
-            document.getElementById("div").style.backgroundColor = `#` + `${txt}`;
-                           }
+function genfun(){
+
+        const q = document.getElementById("ucase").checked ;
+        const w = document.getElementById("lcase").checked ;
+        const e = document.getElementById("ncase").checked ;
+        const r = document.getElementById("scase").checked ;
+        const length1 = document.getElementById("lencase") ;
+
+        const LowerCase = "abcdefghijklmnopqrstuvwxyz" ;
+        const UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const Number = "0123456789" ;
+        const Symbol = "!@#$%^&*()";
+      
+       let allowchars = "" ;
+       let Password ="" ;
+       allowchars += q ? UpperCase : "" ;
+       allowchars += w ? LowerCase : "" ;
+       allowchars += e ? Number : "" ;
+       allowchars += r ? Symbol : "" ;
+
+        for(let i =0; i<length1.value ; i++ ){
+           let randomNum = Math.floor(Math.random()*(allowchars.length));
+            Password += allowchars[randomNum] ;
+        }
+        if( q==false && w==false && e==false && r==false ){
+            document.getElementById("result").textContent="Please Select A ChecBox" ;
+        }
+
+       
+       if( q==false && w==false && e==false && r==false ){
+            document.getElementById("result").textContent="Please Select A CheckBox !!!" ;
+        }
+        else{
+            document.getElementById("result").textContent=` ${Password}` ;
+        }
+    }
